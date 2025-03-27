79011815e8463ea46e8691cc492e146d55a2d395
TinyFox <106698391+Ace-Kyle@users.noreply.github.com>
2025-03-27 08:09:14 +0700

    done: week4
    - exercise 11: get N random elements from array
    - exercise 12: convert date string (separator by "/") into normal format.
    Example:
    30/04/2025 -> 30 April 2025

---
+++ src/bt_tuan4/Test.java
@@ -0,0 +1,32 @@
+package bt_tuan4;
+
+class Test {
+
+    public static void main(String[] args) {
+        //bai 11:
+        int[] a = {1, 2, 3, 4, 5,6, 7, 8, 9, 10};
+        Week4.getRandomFromArray(a, 3);
+        Week4.getRandomFromArray(a, 5);
+        Week4.getRandomFromArray(a, 10);
+
+        //bai 12:
+        String date = "27/03/2025";
+        String date2 = "30/04/2025";
+        String date3 = "01/05/2025";
+        System.out.println(Week4.convertDateNumberToDateString(date));
+        System.out.println(Week4.convertDateNumberToDateString(date2));
+        System.out.println(Week4.convertDateNumberToDateString(date3));
+
+        //bai 13: Tokenizer
+        String testString = "Toi la sinh vien khoa cong nghe thong tin";
+        String testString2 = "Lap trinh nang cao";
+        Week4.printInfoAboutString(testString);
+
+        //bai 14: encryption by column
+        String encrytionString = Week4.encryptionByColumn(testString, 5);
+        char testChar = 'c';
+        System.out.println("Encrytion char: "+Week4.cipherHelper(testChar, 5));
+        System.out.println("Decryption char: " + Week4.cipherHelper(Week4.cipherHelper(testChar, 5), 5));
+
+    }
+}
